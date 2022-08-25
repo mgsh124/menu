e71ae67b89765973463936e3019eb85d4ed3d63b
huyluong <mgsh124@gmail.com>
2022-08-24 20:51:23 -0400

    Chirpus Pizza initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Category } from './enums/category';
+import { MenuItem } from './interfaces/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+
+  menuItem: MenuItem[] = [
+    {item: 'Chicken Fingers', category: Category[Category.Dinner], price: 11.99},
+    {item: 'Pizza', category: Category[Category.Dinner], price: 11.99},
+    {item: 'Wings', category: Category[Category.Sides], price: 8.99},
+    {item: 'Breadsticks', category: Category[Category.Sides], price: 4.99},
+    {item: 'Ceasar Salad', category: Category[Category.Salads], price: 5.99},
+    {item: 'Cinnamon Rolls', category: Category[Category.Dessert], price: 8.99}
+  ];
 }
